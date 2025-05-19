0d02dc288d979e667c0ea67dfc933141941c601b
Carmelo Buelvas <cbuelvasc@gmail.com>
2025-05-19 11:17:22 -0500

    Enhance GitHub Action for deploying to AWS S3 by adding configurable inputs for bucket name, region, and distribution folder; update deployment workflow to utilize these inputs and set AWS credentials as environment variables.

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-function run() {
+async function run() {
+    // 1)  Get some inputs values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2)  Upload the files to the S3 bucket
+    const s3Uri = `s3://${bucket}`;
+    const upload = await exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice(`Uploaded ${distFolder} to ${s3Uri}`);
+
     core.notice('Hello from my custom JavaScript action!');
 }
 
